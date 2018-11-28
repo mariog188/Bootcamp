3bbaf488a569dff611907a29faa747a3e9d662ac
Mario Gutierrez <mario.gutierrez@yuxiglobal.com>
2018-11-28 08:38:52 -0500

    Tabs on tslint.json

--- src/app/dashboard/card/card.component.spec.ts
+++ src/app/dashboard/card/card.component.spec.ts
@@ -1,28 +1,33 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
 import { CardComponent } from './card.component';
+import { DebugElement } from '@angular/core';
+/* tslint:disable:no-unused-variable */
+import {
+	async,
+	ComponentFixture,
+	TestBed,
+} from '@angular/core/testing';
+
 
 describe('CardComponent', () => {
-  let component: CardComponent;
-  let fixture: ComponentFixture<CardComponent>;
+	let component: CardComponent;
+	let fixture: ComponentFixture<CardComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(
+		async(() => {
+			TestBed.configureTestingModule({
+				declarations: [ CardComponent ],
+			}).compileComponents();
+		}),
+	);
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(CardComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
