600bffe23aed4af90ae065feeb90838ce750263c
Mario Gutierrez <mario.gutierrez@yuxiglobal.com>
2018-11-28 08:24:36 -0500

    Inputs and outputs (communication between components)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 	featureImage;
+	images = [
+		'https://media3.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.gif?cid=3640f6095bfdd67944705454459725a9',
+		'https://media.giphy.com/media/yjGdFXjeQsDqJNSzE4/giphy.gif',
+		'https://media.giphy.com/media/mokQK7oyiR8Sk/giphy.gif',
+	];
 
 	showImage(featureImage) {
 		this.featureImage = featureImage;
